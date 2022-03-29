7dd93f805ed545ba6231e608eca1994fab103de8
WaffleTG <toros.gergo@students.jedlik.eu>
2022-03-29 11:34:16 +0200

    a

---
+++ script.js
@@ -0,0 +1,18 @@
+var activ = 0;
+
+function roles(a){
+    var top = document.querySelector("#top")
+    var jg = document.querySelector("#jg")
+    var mid = document.querySelector("#mid")
+    var bot = document.querySelector("#bot")
+    var sup = document.querySelector("#sup")
+
+    switch(a) {
+        case 0:
+            if(top.classList.contains("active") == true){
+                break
+            } else (
+
+            )
+    }
+}
