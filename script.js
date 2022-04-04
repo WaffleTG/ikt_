9c2acd8f9ddd8d2125aa3661bdbbd8afdbbcaca2
WaffleTG <toros.gergo@students.jedlik.eu>
2022-04-04 14:20:25 +0200

    main

--- script.js
+++ script.js
@@ -1,18 +1,106 @@
-var activ = 0;
-
 function roles(a){
     var top = document.querySelector("#top")
-    var jg = document.querySelector("#jg")
+    var jng = document.querySelector("#jg")
     var mid = document.querySelector("#mid")
     var bot = document.querySelector("#bot")
-    var sup = document.querySelector("#sup")
+    var sup = document.querySelector("#sup")  
+
+    var topc = document.querySelector(".champ_row_0")
+    var jngc = document.querySelector(".champ_row_1")
+    var midc = document.querySelector(".champ_row_2")
+    var botc = document.querySelector(".champ_row_3")
+    var supc = document.querySelector(".champ_row_4")  
 
     switch(a) {
         case 0:
             if(top.classList.contains("active") == true){
-                break
-            } else (
+                break;
+            } else {
+                top.classList.add("active");
+                jng.classList.remove("active");
+                mid.classList.remove("active");
+                bot.classList.remove("active");
+                sup.classList.remove("active");
 
-            )
+                topc.classList.remove("hidden");
+                jngc.classList.add("hidden");
+                midc.classList.add("hidden");
+                botc.classList.add("hidden");
+                supc.classList.add("hidden");
+
+                break;
+            }
+        case 1:
+            if(jng.classList.contains("active ")== true){
+                break;
+            } else {
+                top.classList.remove("active");
+                jng.classList.add("active");
+                mid.classList.remove("active");
+                bot.classList.remove("active");
+                sup.classList.remove("active");
+
+                topc.classList.add("hidden");
+                jngc.classList.remove("hidden");
+                midc.classList.add("hidden");
+                botc.classList.add("hidden");
+                supc.classList.add("hidden");
+
+                break;
+            }
+        case 2:
+            if(mid.classList.contains("active") == true){
+                break;
+            } else {
+                top.classList.remove("active");
+                jng.classList.remove("active");
+                mid.classList.add("active");
+                bot.classList.remove("active");
+                sup.classList.remove("active");
+
+                topc.classList.add("hidden");
+                jngc.classList.add("hidden");
+                midc.classList.remove("hidden");
+                botc.classList.add("hidden");
+                supc.classList.add("hidden");
+
+
+                break;
+            }
+        case 3:
+            if(bot.classList.contains("active") == true){
+                break;
+            } else {
+                top.classList.remove("active");
+                jng.classList.remove("active");
+                mid.classList.remove("active");
+                bot.classList.add("active");
+                sup.classList.remove("active");
+
+                topc.classList.add("hidden");
+                jngc.classList.add("hidden");
+                midc.classList.add("hidden");
+                botc.classList.remove("hidden");
+                supc.classList.add("hidden");
+
+                break;
+            }
+        case 4:
+        if(sup.classList.contains("active") == true){
+            break;
+        } else {
+            top.classList.remove("active");
+            jng.classList.remove("active");
+            mid.classList.remove("active");
+            bot.classList.remove("active");
+            sup.classList.add("active");
+
+            topc.classList.add("hidden");
+            jngc.classList.add("hidden");
+            midc.classList.add("hidden");
+            botc.classList.add("hidden");
+            supc.classList.remove("hidden");
+            break;
+        }
     }
 }
